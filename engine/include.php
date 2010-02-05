39b862288f0869f95d580450fc37a52d040cbb53
mm <mm@73ad06da-0985-4a90-93d2-e6fe430330fc>
2010-02-05 14:41:04 +0000

    List of files to include update.
    git-svn-id: svn://nlp.pwr.wroc.pl/czuk/gpw/web@22 73ad06da-0985-4a90-93d2-e6fe430330fc

--- engine/include.php
+++ engine/include.php
@@ -11,9 +11,9 @@ require_once($conf_global_path . '/pear/FirePHPCore/fb.php');
 require_once($conf_global_path . '/include/CPage.php');
 require_once($conf_global_path . '/include/CAction.php');
 require_once($conf_global_path . '/include/CTextAligner.php');
+require_once($conf_global_path . '/include/CTeiFormater.php');
 
 require_once($conf_global_path . '/include/report_reformat.php');
-require_once($conf_global_path . '/include/tei_formater.php');
 
 chdir("..");
 
