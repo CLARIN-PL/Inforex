48154fbc7945681520a105874c74d1f240432503
mm <mm@73ad06da-0985-4a90-93d2-e6fe430330fc>
2010-02-19 13:54:20 +0000

    Script to split IOB file into folds.
    git-svn-id: svn://nlp.pwr.wroc.pl/czuk/gpw/web@40 73ad06da-0985-4a90-93d2-e6fe430330fc

---
+++ console/split.php
@@ -0,0 +1,88 @@
+<?php
+/* 
+ * ---
+ * 
+ * ---
+ * Created on 2010-02-15
+ * Michał Marcińczuk <marcinczuk@gmail.com> [czuk.eu]
+ */
+ 
+/******************** set configuration   *********************************************/
+$config = null;
+$config->folds = 10;
+$config->ratio = "2"; // training : testing
+$config->input = $argv[1]; 
+
+if ($config->input == "" ) 
+	die ("Incorrect argument. Expected one of the following formats:\n" .
+			"php split.php <filename>      // split given file name\n\n");
+if (!file_exists($config->input))
+	die ("File '{$config->input}' does not exist\n");  
+
+/******************** misc function       *********************************************/
+
+function save_fold($config, $folds, $fold_num){
+	$lines = "";
+	for ($i=0; $i<count($folds); $i++)
+		if ($i+1 != $fold_num)
+			$lines .= implode("\n", $folds[$i]) . "\n\n";
+	file_put_contents($config->input . ".fold-{$fold_num}.train", $lines);
+	file_put_contents($config->input . ".fold-{$fold_num}.test", implode("\n", $folds[$fold_num-1]));	
+}
+			
+/******************** main function       *********************************************/
+// Pricess all files in a folder
+function main ($config){
+	$count = 0;
+	$lines = explode("\n", file_get_contents($config->input));
+	foreach ($lines as $line)
+		if (strpos($line, " B-")!==false)
+			$count++;
+	echo "Number of annotations: $count\n";
+	
+	$count_train_set = 0;
+	$count_in_sentence = 0;
+	$examples_in_fold = $count/$config->folds;
+		
+	// Split into folds
+	$folds = array();
+	for ($i=0; $i<$config->folds; $i++)
+		$folds[$i] = array();
+		
+	$i = 0;
+	$fold_num = 0;
+	$count_total = 0;
+	echo "======== =========== =========\n";
+	echo "Fold     Annotations Sentences\n";
+	echo "======== =========== =========\n";
+	while ($i<count($lines)){
+		$this_fold = 0;
+		while ($i<count($lines) && !($count_total >=$count*($fold_num+1)/$config->folds && trim($lines[$i])=='') ){
+			if (strpos($lines[$i], " B-")!==false){
+				$this_fold++;
+				$count_total++;
+			}
+			$folds[$fold_num][] = $lines[$i];
+			$i++;
+		}
+		// Skip an empty line.
+		$i++;		
+		echo sprintf("Fold %2d %3d            %6d \n", $fold_num+1, $this_fold, count($folds[$fold_num]));
+		$fold_num++;
+		if ( $fold_num == $config->folds ){
+			while($i<count($lines))
+				$folds[$fold_num-1][] = $lines[$i++];
+		}
+	}
+	echo "======== =========== =========\n";
+
+	for ($n=1; $n<=10; $n++){
+		echo " Saving fold $n\n";
+		save_fold($config, $folds, $n);
+	}
+}
+
+/******************** main invoke         *********************************************/
+main($config);
+
+?>
