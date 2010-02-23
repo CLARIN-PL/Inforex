c6c004a854cac96a98a3d9d50a372b5524fee53c
mm <mm@73ad06da-0985-4a90-93d2-e6fe430330fc>
2010-02-23 15:57:35 +0000

    NER - input text is now tokenized.
    git-svn-id: svn://nlp.pwr.wroc.pl/czuk/gpw/web@46 73ad06da-0985-4a90-93d2-e6fe430330fc

--- engine/pages/ner.php
+++ engine/pages/ner.php
@@ -13,7 +13,7 @@ class Page_ner extends CPage{
 			$client = new SoapClient(TAKIPI_WSDL); 
 
 			// Send a request 
-			$request = $client->SplitIntoSentences($content, "XML", false);			
+			$request = $client->Tag($content, "XML", false);			
 			$token = $request->msg; 
 			$status = $request->status;
 		
@@ -34,12 +34,29 @@ class Page_ner extends CPage{
 			    } 
 			}
 
-			preg_match_all('/<chunk type="s">(.*?)<\/chunk>/s', $result->msg, $matches, PREG_SET_ORDER);
-			$content_marked = "";
-			foreach ($matches as $match){
-				$sentence = $match[1];
+			$takipiDocument = TakipiDocument::createFromText("<doc>".$result->msg."</doc>");
+	
+			// Naprawa skrótów ps.
+			$to_remove = array();
+			foreach ($takipiDocument->sentenceEnds as $end){
+				if ($end>0 && $takipiDocument->tokens[$end-1]->orth=="ps")
+					$to_remove[] = $end; 
+			}
+			$takipiDocument->sentenceEnds = array_diff($takipiDocument->sentenceEnds, $to_remove);
+	
+			//preg_match_all('/<chunk type="s">(.*?)<\/chunk>/s', $result->msg, $matches, PREG_SET_ORDER);
+			//$content_marked = "";
+			$begin = 0;
+			foreach ($takipiDocument->sentenceEnds as $end){
+				$sentence = "";
+				for ($i=$begin; $i<=$end; $i++) $sentence .= $takipiDocument->tokens[$i]->orth . " ";
+				$begin = $end + 1;
+				
+				$sentence = trim($sentence);
 				$path = PATH_ENGINE . "/ner";
-				$cmd = "LANG=en_US.utf-8; java -cp {$path}/lingpipe-3.8.2.jar:{$path}/neDemo.jar RunChunker {$path}/gpw-person.model " . '"' . str_replace('"', '\"', $sentence) . '"';
+				//$model = "gpw-person.model";
+				$model = "gpw2004.model";
+				$cmd = "LANG=en_US.utf-8; java -cp {$path}/lingpipe-3.8.2.jar:{$path}/neDemo.jar RunChunker {$path}/{$model} " . '"' . str_replace('"', '\"', $sentence) . '"';
 				$result = exec($cmd);
 				
 				$matches = array();
@@ -47,12 +64,36 @@ class Page_ner extends CPage{
 				
 				$result = $sentence;				
 				foreach (array_reverse($matches) as $match){
+					$annotation_text = trim(mb_substr($sentence, $match[1], $match[2]-$match[1]+1));
 					$result = mb_substr($result, 0, $match[2])."</b>".mb_substr($result, $match[2]);
-					$result = mb_substr($result, 0, $match[1])."<b style='background: yellow'>".mb_substr($result, $match[1]);
+					if (ner_filter($annotation_text))
+						$result = mb_substr($result, 0, $match[1])."<b style='background: yellow'>".mb_substr($result, $match[1]);
+					else
+						$result = mb_substr($result, 0, $match[1])."<b style='background: red'>".mb_substr($result, $match[1]);						
 				}
 
-				$content_marked .= $result; 				
+				$content_marked .= $result ."<hr/>"; 				
+				
 			}
+//			foreach ($matches as $match){
+//				$sentence = $match[1];
+//				$path = PATH_ENGINE . "/ner";
+//				//$model = "gpw-person.model";
+//				$model = "gpw2004.model";
+//				$cmd = "LANG=en_US.utf-8; java -cp {$path}/lingpipe-3.8.2.jar:{$path}/neDemo.jar RunChunker {$path}/{$model} " . '"' . str_replace('"', '\"', $sentence) . '"';
+//				$result = exec($cmd);
+//				
+//				$matches = array();
+//				preg_match_all("/([0-9]+)-([0-9]+):PERSON/", $result, $matches, PREG_SET_ORDER);
+//				
+//				$result = $sentence;				
+//				foreach (array_reverse($matches) as $match){
+//					$result = mb_substr($result, 0, $match[2])."</b>".mb_substr($result, $match[2]);
+//					$result = mb_substr($result, 0, $match[1])."<b style='background: yellow'>".mb_substr($result, $match[1]);
+//				}
+//
+//				$content_marked .= $result; 				
+//			}
 		}
 		$this->set('content', $content);
 		$this->set('result', $content_marked);
