622eca42c48a7c0900bd25fc945333dc8278c1e6
mm <mm@73ad06da-0985-4a90-93d2-e6fe430330fc>
2010-02-23 22:15:08 +0000

    Include update
    git-svn-id: svn://nlp.pwr.wroc.pl/czuk/gpw/web@48 73ad06da-0985-4a90-93d2-e6fe430330fc

--- engine/include.php
+++ engine/include.php
@@ -8,12 +8,16 @@ require_once($conf_global_path . '/pear/HTTP/Session2.php');
 require_once($conf_global_path . '/pear/HTML/Select.php');
 require_once($conf_global_path . '/pear/FirePHPCore/fb.php');
 
+require_once($conf_global_path . '/include/anntakipi/ixtTakipiReader.php');
+require_once($conf_global_path . '/include/anntakipi/ixtTakipiDocument.php');
+
 require_once($conf_global_path . '/include/CPage.php');
 require_once($conf_global_path . '/include/CAction.php');
 require_once($conf_global_path . '/include/CTextAligner.php');
 require_once($conf_global_path . '/include/CTeiFormater.php');
 
 require_once($conf_global_path . '/include/report_reformat.php');
+require_once($conf_global_path . '/include/ner_filter.php');
 
 chdir("..");
 
