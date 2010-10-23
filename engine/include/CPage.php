f6da53c3163b9aa752753b95b042d70533ba74ab
mm <mm@73ad06da-0985-4a90-93d2-e6fe430330fc>
2010-10-23 08:56:14 +0000

    Comments added
    git-svn-id: svn://nlp.pwr.wroc.pl/czuk/gpw/web@162 73ad06da-0985-4a90-93d2-e6fe430330fc

--- engine/include/CPage.php
+++ engine/include/CPage.php
@@ -1,4 +1,39 @@
 <?php
+/**
+ * Class representsa a page displaying a content to a user.
+ *
+ * @tutorial
+ * == How to set access to a page ==
+ * 1. The access parameters must be set in the derived class.
+ * 2. Level of access:
+ *    a) public access -- any user can see the content,
+ *       > $isSecure = false;
+ *       
+ *    b) access only for the administrators
+ *    	 > $isSecure = true;
+ *       > $roles = array(); // no role defined
+ *       
+ *    c) any logged in user and administrators
+ *       > $isSecure = true;
+ *       > $roles = array('loggedin');
+ *       
+ *    d) users with a custom role and administrators
+ *       > $isSecure = true;
+ *       > $roles = array('page_unique_role', 'any_other_role');
+ *       User must have at least one of the defined rules to see the content.
+ *
+ * 3. Defining user roles
+ *    The role should be registered in the database in the tabel 'roles'. The roles are granted to a user by inserting an
+ *    entry into the table 'user_roles'. An entry (1, 'admin') means, that a user with id=1 has role 'admin'.
+ * 4. Dynamic access
+ * 	  Any other dynamic access rules can be defined in the function 'checkPermission()'. The additional rules does not aplly
+ *    the the administrators but only to users with 'loggedin' or any other defined custom role.
+ * 5. Corpus subpage
+ *    Any page that is a corpus subpage can have a predefined role 'corpus_owner'. The role allows the corpus owner to see the 
+ *    page content with no respect to other access requirements (roles and checkPermission function).
+ *       
+ * @author Michał Marcińczuk
+ */
 class CPage {
 	
 	var $template;
@@ -10,18 +45,36 @@ class CPage {
 		$this->set('RELEASE', RELEASE);		
 	}
 	
+	/**
+	 * Check any custom permission to the page.
+	 * @return true if user can access the page
+	 */
 	function checkPermission(){
 		return true;
 	}
 	
+	/**
+	 * Assign a variable. The variable can be accessed from the smarty template.
+	 * @param $name -- variable name
+	 * @param $value -- variable value
+	 */
 	function set($name, $value){
 		$this->template->assign($name, $value);
 	}
 	
+	/**
+	 * Assign a variable by a reference. The variable can be accessed from the smarty template.
+	 * @param $name -- variable name
+	 * @param $object -- variable reference to an object
+	 */
 	function set_by_ref($name, &$object){
 		$this->template->assign_by_ref($name, $object);		
 	}
 
+	/**
+	 * Get an variable value assign to the page.
+	 * @param $name -- a variable name
+	 */
 	function get($name){
 		if (isset($this->template->_tpl_vars[$name]))
 			return $this->template->_tpl_vars[$name];
@@ -29,23 +82,42 @@ class CPage {
 			return null;
 	}
 		
+	/**
+	 * Assign to the page a table of variables.
+	 * @param $variables -- a table of variables
+	 */
 	function setVariables($variables){
 		foreach ($variables as $k=>$m)
 			$this->set($k, $m);
 	}
 
+	/**
+	 * Assign to the page a table of variable references. 
+	 * @param $variables -- a table of variable references
+	 */
 	function setRefs($variables){
 		foreach ($variables as $k=>$m)			
 			$this->set_by_ref($k, $m);
 	}
 		
+	/**
+	 * Generate page content. This function must be overloaded in the derived function.
+	 */
 	function execute(){}
 
+	/**
+	 * Display page content using given template.
+	 * @param $template_name -- name of a template. The template should save as a file /template/page_template_name.tpl 
+	 */
 	function display($template_name){
 		global $config;
 		$this->template->display($config->path_engine . "/templates/page_{$template_name}.tpl");
 	}
 		
+	/**
+	 * Make the browser to redirect the user to another page.
+	 * @param $url -- location where the user should be redirected.
+	 */
 	function redirect($url){
 		header("Location: $url");
 		ob_clean();
