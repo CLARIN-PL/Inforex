c22425312ca90c43be8a95aad9e552ff2c1e9554
mm <mm@73ad06da-0985-4a90-93d2-e6fe430330fc>
2010-11-16 08:54:56 +0000

    Script to load scans
    git-svn-id: svn://nlp.pwr.wroc.pl/czuk/gpw/web@186 73ad06da-0985-4a90-93d2-e6fe430330fc

---
+++ console/lps/load-scans.php
@@ -0,0 +1,73 @@
+<?
+
+$dryrun = true;
+
+if (isset($argv[1]) && $argv[1]=="make")
+	$dryrun = false;
+else{
+	print "This is only dry run.\nTo process run with 'php load-scans.php make' parameter\n";
+}
+
+$folder = "/home/czuk/workdir/lps/scans/images";
+$target = "/home/czuk/nlp/eclipse/workspace_inforex/inforex_web/secured_data/images";
+
+$files = array();
+
+// Load scans
+if (is_dir($folder)) {
+    if ($dh = opendir($folder)) {
+        while (($file = readdir($dh)) !== false) {
+        	if ($file != ".." && $file!=".")
+            	$files[] = $file;
+        }
+        closedir($dh);
+    }
+}
+
+sort($files);
+
+$files_by_names = array();
+foreach ($files as $file){
+	list($name, $tail) = explode("-", $file);
+	$files_by_names[$name][] = $file;
+}
+
+if (!$dryrun){
+	mysql_connect("localhost", "root", "krasnal");
+	mysql_select_db("gpw");
+	mysql_query("SET CHARACTER SET utf8");
+}
+
+foreach ($files_by_names as $name=>$files){
+	$sql_report = sprintf("INSERT INTO reports (date, title, status, user_id, corpora) VALUES('%s', '%s', 2, 1, 3)", date("Y-m-d"), mysql_escape_string($name));
+	if (!$dryrun)
+		mysql_query($sql_report);
+	else
+		print $sql_report . "\n";
+	$report_id = mysql_insert_id();
+	
+	$n = 1;
+	foreach ($files as $file){
+		$sql = sprintf("INSERT INTO images (corpus_id, original_name, hash_name) VALUES(3, '%s', '%s');", mysql_escape_string($file), mysql_escape_string($file));
+		if (!$dryrun)
+			mysql_query($sql);
+		else
+			print $sql . "\n";
+		$image_id = mysql_insert_id();
+		
+		$sql = sprintf("INSERT INTO reports_and_images (report_id, image_id, position) VALUES(%d, %d, %d)", $report_id, $image_id, $n++);
+		if (!$dryrun)
+			mysql_query($sql);
+		else
+			print $sql . "\n";
+			
+		if (!$dryrun)
+		{
+			$image_name = $image_id."_".$file;
+			copy("$folder/$file", "$target/$image_name");
+		}
+		
+	}
+	
+}
+?>
