34f52ad0447a30a9b33215ab175d237390f05590
Michał Marcińczuk <marcinczuk@gmail.com>
2011-06-28 13:53:49 +0200

    Blokowanie przycisku i wskaźnik postępu dodany do perpektywy tokenizacji

--- public_html/js/page_report_tokenization.js
+++ public_html/js/page_report_tokenization.js
@@ -4,6 +4,12 @@ $(function(){
 	});
 	
 	$("#takipiwsProcess").click(function(){
+		
+		var button = this;
+		
+		$(button).after("<img class='ajax_indicator' src='gfx/ajax.gif'/>");
+		$(button).attr("disabled", "disabled");
+		
 		$.ajax({
 			type: 	'POST',
 			url: 	"index.php",
@@ -13,13 +19,17 @@ $(function(){
 					},
 			success:function(data){
 						if ( data.success ){
-							$("#messageBox").text("Tokenization successfully completed. Reload page to see result.");
+							$("#messageBox").html("<div class='info'>Tokenization successfully completed. Reload page to see result.</div>");
 						}
 						else
-							$("#messageBox").text("Tokenization failed. "+data.error);
+							$("#messageBox").html("<div class='error'>Tokenization failed. "+data.error+"</div>");
+						$(button).removeAttr("disabled");
+						$(".ajax_indicator").remove();
 					},
 			error: function(request, textStatus, errorThrown){
 						$("#messageBox").text("Tokenization failed.");
+						$(button).removeAttr("disabled");
+						$(".ajax_indicator").remove();
 					},
 			dataType:"json"
 		});		
