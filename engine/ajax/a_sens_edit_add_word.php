78a6ce6964a1fd3a405616081fd2a2e9b316a6f4
Marcin Ptak <mp.marcin.ptak@gmail.com>
2012-02-06 14:01:48 +0100

    #3582

--- engine/ajax/a_sens_edit_add_word.php
+++ engine/ajax/a_sens_edit_add_word.php
@@ -5,7 +5,7 @@ class Ajax_sens_edit_add_word extends CPage {
 		$name = $_POST['wordname'];
 		$wsd_name = "wsd_" . $name;
 		
-		$sql = " SELECT * FROM annotation_types WHERE name like '" . $wsd_name . "'";
+/*		$sql = " SELECT * FROM annotation_types WHERE name like '" . $wsd_name . "'";
 		
 		$result = $db->fetch_one($sql);
 		if(count($result)){
@@ -13,18 +13,25 @@ class Ajax_sens_edit_add_word extends CPage {
 			echo json_encode(array("error"=>$error_msg));
 			return;
 		}
-		try{
-			$sql = "INSERT INTO annotation_types (name, group_id, annotation_subset_id) VALUES (?, 2, 21)";
-			$db->execute($sql, array($wsd_name));
-			//$mdb2->isError
-						
-			$sql = "INSERT INTO annotation_types_attributes (annotation_type, name, type) VALUES (?, 'sense', 'radio')";
-			$db->execute($sql, array($wsd_name));
-				
-			$rows_id = $mdb2->lastInsertID();
-		}catch(Exception $ex){
-			print "\n\n!! " . $ex->getMessage() . " !!\n\n";
+*/		$sql = "INSERT INTO annotation_types (name, group_id, annotation_subset_id) VALUES (?, 2, 21)";
+			
+		$db->execute($sql, array($wsd_name));
+		
+//		print_r($db->mdb2->errorInfo());
+		$error = $db->mdb2->errorInfo();
+		if(isset($error[0])){
+			$error_msg = 'Word ' . $name . ' alredy exist';
+			echo json_encode(array("error"=>$error_msg));
+			return;
 		}
+		
+			
+		$sql = "INSERT INTO annotation_types_attributes (annotation_type, name, type) VALUES (?, 'sense', 'radio')";
+		$db->execute($sql, array($wsd_name));
+		
+//		print_r($db->mdb2->errorInfo());
+		
+		$rows_id = $mdb2->lastInsertID();
 		echo json_encode(array("success" => 1, "rows_id" => $rows_id));
 	}	
 }
