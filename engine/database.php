275826ced99db1f4bd759f33bc080570c15d0b07
Michał Marcińczuk <marcinczuk@gmail.com>
2013-02-25 15:00:08 +0100

    Poprawki zapytań SQL.

--- engine/database.php
+++ engine/database.php
@@ -107,10 +107,10 @@ function db_fetch_rows($sql, $args = null){
 	}
 	if ($args == null){
 		if (PEAR::isError($r = $mdb2->query($sql)))
-			die("<pre>{$r->getUserInfo()}</pre>");
+			throw new Exception("<pre>{$r->getUserInfo()}</pre>");
 	}else{
 		if (PEAR::isError($sth = $mdb2->prepare($sql)))
-			die("<pre>{$sth->getUserInfo()}</pre>");
+			throw new Exception("<pre>{$sth->getUserInfo()}</pre>");
 		$r = $sth->execute($args);
 		if ($sql_log){
 			fb($args, "SQL DATA");
@@ -127,10 +127,10 @@ function db_execute($sql, $args=null){
 	}
 	if ($args == null){
 		if (PEAR::isError($r = $mdb2->query($sql)))
-			die("<pre>{$r->getUserInfo()}</pre>");
+			throw new Exception("<pre>{$r->getUserInfo()}</pre>");
 	}else{
 		if (PEAR::isError($sth = $mdb2->prepare($sql)))
-			die("<pre>{$sth->getUserInfo()}</pre>");
+			throw new Exception("<pre>{$sth->getUserInfo()}</pre>");
 		$sth->execute($args);
 		if ($sql_log){
 			fb($args, "SQL DATA");
@@ -153,10 +153,10 @@ function db_fetch($sql, $args=null){
 	$args = $args == null ? array() : $args;
 	
 	if (PEAR::isError($sth = $mdb2->prepare($sql)))
-		die("<pre>{$sth->getUserInfo()}</pre>");
+		throw new Exception("<pre>{$sth->getUserInfo()}</pre>");
 		
 	if (PEAR::isError($r = $sth->execute($args)))
-		die("<pre>{$r->getUserInfo()}</pre>");	
+		throw new Exception("<pre>{$r->getUserInfo()}</pre>");	
 	return $r->fetchRow(MDB2_FETCHMODE_ASSOC);			
 }
 
@@ -168,13 +168,13 @@ function db_fetch_one($sql, $args=null){
 	}
 	if ($args == null){
 		if (PEAR::isError($r = $mdb2->query($sql)))
-			die("<pre>{$r->getUserInfo()}</pre>");		
+			throw new Exception("<pre>{$r->getUserInfo()}</pre>");		
 	}else{
 		if (!is_array($args)){
 			$args = array($args);
 		}
 		if (PEAR::isError($sth = $mdb2->prepare($sql)))
-			die("<pre>{$sth->getUserInfo()}</pre>");
+			throw new Exception("<pre>{$sth->getUserInfo()}</pre>");
 		$r = $sth->execute($args);
 		if ($sql_log){
 			fb($args, "SQL DATA");
