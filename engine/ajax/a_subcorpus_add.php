326cb7c80d6fcabe206bc3cccde56aba8a13939c
Michał Marcińczuk <marcinczuk@gmail.com>
2013-03-05 12:17:12 +0100

    Poprawka w dodawaniu podkorpusu.

--- engine/ajax/a_subcorpus_add.php
+++ engine/ajax/a_subcorpus_add.php
@@ -12,10 +12,15 @@ class Ajax_subcorpus_add extends CPage {
 		global $db, $corpus, $mdb2;
 
 		$sql = "INSERT INTO corpus_subcorpora (corpus_id, name, description) VALUES (?, ?, ?) ";
+		$corpus_id = $corpus['id'];
+		$corpus_name = strval($_POST['name_str']);
+		$corpus_desc = strval($_POST['desc_str']);
+				
 		ob_start();
-		$db->execute($sql, array($corpus['id'], $_POST['name_str'], $_POST['desc_str']));
+		$db->execute($sql, array($corpus_id, $corpus_name, $corpus_desc));
 		$error_buffer_content = ob_get_contents();
 		ob_clean();
+		
 		if(strlen($error_buffer_content))
 			echo json_encode(array("error"=> "Error: ". $error_buffer_content));
 		else{
