2e9cc1aa1b3fe68cd5b59e1dd168fec8a1a1502a
akaczmarek <adamjankaczmarek@gmail.com>
2013-08-29 13:37:41 +0200

    CDbReportEvent

---
+++ engine/include/database/CDbReportEvent.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Part of the Inforex project
+ * Copyright (C) 2013 Michał Marcińczuk, Jan Kocoń, Marcin Ptak
+ * Wrocław University of Technology
+ * See LICENCE
+ */
+
+class DbReportEvent{
+	
+	static function addEvent($report_id, $event_type_id, $user_id){
+		global $db;
+		$sql = "INSERT INTO reports_events (report_id, event_type_id, user_id, creation_time) " .
+				"VALUES ($report_id, $event_type_id, $user_id, now())";
+		
+		db_execute($sql);
+		//$db->execute($sql);
+	}
+	
+}
