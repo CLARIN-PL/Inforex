1f90dab94fe76b2fb9340017a158000409a17e7f
Michał Marcińczuk <marcinczuk@gmail.com>
2013-11-12 11:35:30 +0100

    Poprawiony sql do aktualizacji bazy.

--- documentations/sql/update_031_tokens_tags_ctags_pos.sql
+++ documentations/sql/update_031_tokens_tags_ctags_pos.sql
@@ -1,13 +1,12 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
 CREATE TABLE IF NOT EXISTS `tokens_tags_ctags` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `ctag` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-ALTER TABLE  `tokens_tags_optimized` ADD  `ctag_id` INT NOT NULL ;
+ALTER TABLE `tokens_tags_ctags` ADD INDEX ( `ctag` ) ;
+
+ALTER TABLE  `tokens_tags_optimized` ADD `ctag_id` INT;
 ALTER TABLE  `tokens_tags_optimized` ADD FOREIGN KEY (  `ctag_id` ) REFERENCES  `tokens_tags_ctags` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 INSERT INTO `tokens_tags_ctags`(`ctag`) SELECT DISTINCT ctag FROM `tokens_tags_optimized`;
@@ -16,3 +15,4 @@ UPDATE `tokens_tags_optimized` tto JOIN `tokens_tags_ctags` ttc USING(ctag) SET 
 ALTER TABLE  `tokens_tags_optimized` ADD  `pos` VARCHAR( 20 ) NOT NULL ;
 UPDATE `tokens_tags_optimized` SET pos = SUBSTRING_INDEX( SUBSTRING_INDEX(ctag,  ':', 1 ) ,  ':', -1 ) 
 ALTER TABLE `tokens_tags_optimized`  DROP `base`,  DROP `ctag`;
+ALTER TABLE `tokens_tags_optimized` CHANGE `ctag_id` `ctag_id` INT( 11 ) NOT NULL ;
