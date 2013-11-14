9b56a665cea15b8b7475c4359a6ca478091548e7
Michał Marcińczuk <marcinczuk@gmail.com>
2013-11-14 17:10:00 +0100

    Poprawka dla sql 031

--- documentations/sql/update_031_tokens_tags_ctags_pos.sql
+++ documentations/sql/update_031_tokens_tags_ctags_pos.sql
@@ -13,6 +13,6 @@ INSERT INTO `tokens_tags_ctags`(`ctag`) SELECT DISTINCT ctag FROM `tokens_tags_o
 UPDATE `tokens_tags_optimized` tto JOIN `tokens_tags_ctags` ttc USING(ctag) SET tto.ctag_id = ttc.id;
 
 ALTER TABLE  `tokens_tags_optimized` ADD  `pos` VARCHAR( 20 ) NOT NULL ;
-UPDATE `tokens_tags_optimized` SET pos = SUBSTRING_INDEX( SUBSTRING_INDEX(ctag,  ':', 1 ) ,  ':', -1 ) 
-ALTER TABLE `tokens_tags_optimized`  DROP `base`,  DROP `ctag`;
+UPDATE `tokens_tags_optimized` SET pos = SUBSTRING_INDEX( SUBSTRING_INDEX(ctag,  ':', 1 ) ,  ':', -1 ) ;
+ALTER TABLE `tokens_tags_optimized`  DROP `ctag`;
 ALTER TABLE `tokens_tags_optimized` CHANGE `ctag_id` `ctag_id` INT( 11 ) NOT NULL ;
