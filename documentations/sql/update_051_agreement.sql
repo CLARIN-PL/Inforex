efb062fe62ce980887f27e4e2241cb096115ae58
Michał Marcińczuk <marcinczuk@gmail.com>
2016-10-03 15:19:32 +0200

    Dodanie nowej wartości stage w reports_annotations_optimized.

--- documentations/sql/update_051_agreement.sql
+++ documentations/sql/update_051_agreement.sql
@@ -1,1 +1,3 @@
 UPDATE `corpus_roles` SET description='Edycja anotacji prywatnych' WHERE role='annotate_agreement';
+
+ALTER TABLE `reports_annotations_optimized` CHANGE `stage` `stage` ENUM( 'new', 'final', 'discarded', 'agreement' ) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL DEFAULT 'final'
