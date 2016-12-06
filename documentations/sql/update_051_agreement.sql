1704997374a32ff198a244ddaf50f4328cf21411
Jan Kocon <jan.kocon@pwr.edu.pl>
2016-12-06 14:42:07 +0100

    sql fix

--- documentations/sql/update_051_agreement.sql
+++ documentations/sql/update_051_agreement.sql
@@ -1,6 +1,6 @@
 UPDATE `corpus_roles` SET description='Edycja anotacji prywatnych' WHERE role='annotate_agreement';
 
-ALTER TABLE `reports_annotations_optimized` CHANGE `stage` `stage` ENUM( 'new', 'final', 'discarded', 'agreement' ) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL DEFAULT 'final'
+ALTER TABLE  `reports_annotations_optimized` CHANGE  `stage`  `stage` ENUM(  'new',  'final',  'discarded',  'agreement' ) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL DEFAULT  'final';
 
 ALTER TABLE `corpus_roles` ADD `description_long` TEXT NOT NULL ;
 
