f94692cf8d15b2c5c3774769c8308790fe56a85a
Wojciech Rauk <wojtek.rauk@gmail.com>
2017-10-16 13:33:58 +0200

    added sql for assigning all ctags with 'nkjp' tagset id

--- documentations/sql/update_066_morpho_disamb.sql
+++ documentations/sql/update_066_morpho_disamb.sql
@@ -42,3 +42,12 @@ VALUES (
 
 -- inserting NKJP tagset into `tagsets`
 INSERT INTO `inforex`.`tagsets` (`name`) VALUES ("nkjp");
+
+-- setting tagset_id of all tokens_tags_ctags to that of "nkjp"
+UPDATE  `inforex`.`tokens_tags_ctags` dest,
+(
+SELECT *
+FROM  `inforex`.`tagsets`
+WHERE  `name` LIKE  "nkjp"
+)src
+SET dest.`tagset_id` = src.`tagset_id`;
