5790873e7bc415c9de5503ad0b3fdafbf35230bf
Wojciech Rauk <wojtek.rauk@gmail.com>
2017-10-19 14:59:57 +0200

    fix for array_column function missing

--- engine/pages/report_perspectives/PerspectiveMorphoDisambAgreement.php
+++ engine/pages/report_perspectives/PerspectiveMorphoDisambAgreement.php
@@ -5,7 +5,34 @@
  * Wrocław University of Technology
  * See LICENCE 
  */
- 
+
+if (! function_exists('array_column')) {
+    function array_column(array $input, $columnKey, $indexKey = null) {
+        $array = array();
+        foreach ($input as $value) {
+            if ( !array_key_exists($columnKey, $value)) {
+                trigger_error("Key \"$columnKey\" does not exist in array");
+                return false;
+            }
+            if (is_null($indexKey)) {
+                $array[] = $value[$columnKey];
+            }
+            else {
+                if ( !array_key_exists($indexKey, $value)) {
+                    trigger_error("Key \"$indexKey\" does not exist in array");
+                    return false;
+                }
+                if ( ! is_scalar($value[$indexKey])) {
+                    trigger_error("Key \"$indexKey\" does not contain scalar value");
+                    return false;
+                }
+                $array[$value[$indexKey]] = $value[$columnKey];
+            }
+        }
+        return $array;
+    }
+}
+
 class PerspectiveMorphoDisambAgreement extends CPerspective
 {
     function checkPermission(){
