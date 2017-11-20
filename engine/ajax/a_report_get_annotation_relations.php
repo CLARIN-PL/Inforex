1c3899a14d1cb8deacb105d1498fd0dc8531402b
Mikołaj Szewczyk <mikolaj.m.szewczyk@gmail.com>
2017-11-20 15:03:11 +0100

    Poprawa zapytania SQL w przypadku gdy uzytkownik nie wybierze zadnych zbiorow relacji w anotatorze

--- engine/ajax/a_report_get_annotation_relations.php
+++ engine/ajax/a_report_get_annotation_relations.php
@@ -25,21 +25,25 @@ class Ajax_report_get_annotation_relations extends CPage {
         $relationSetIds = CookieManager::getRelationSets($corpusId);
         $rels_imploded = implode(",", $relationSetIds);
 
-        //Only find relations with appropriate stage.
-        if($annotation_mode == 'relation_agreement' || $annotation_mode == 'agreement'){
-            $annotation_mode = 'agreement';
-            $where_sql = "AND user_id = " . $user['user_id'];
-        }
-		$sql =  "SELECT rr.id, rr.stage, relation_types.name, rr.target_id, rr.user_id, reports_annotations.text, reports_annotations.type " .
-				"FROM ((SELECT * FROM relations WHERE (source_id={$annotation_id} AND stage = '{$annotation_mode}'  {$where_sql})) rr " .
-				"JOIN relation_types  ON rr.relation_type_id=relation_types.id) " .
-				"JOIN reports_annotations ON rr.target_id=reports_annotations.id
+        ChromePhp::log($relationSetIds);
+
+        //If there are no relation sets selected, return an empty array.
+        if(empty($relationSetIds)){
+            return array();
+        } else{
+            //Only find relations with appropriate stage.
+            if($annotation_mode == 'relation_agreement' || $annotation_mode == 'agreement'){
+                $annotation_mode = 'agreement';
+                $where_sql = "AND user_id = " . $user['user_id'];
+            }
+            $sql =  "SELECT rr.id, rr.stage, relation_types.name, rr.target_id, rr.user_id, reports_annotations.text, reports_annotations.type " .
+                "FROM ((SELECT * FROM relations WHERE (source_id={$annotation_id} AND stage = '{$annotation_mode}'  {$where_sql})) rr " .
+                "JOIN relation_types  ON rr.relation_type_id=relation_types.id) " .
+                "JOIN reports_annotations ON rr.target_id=reports_annotations.id
 				 WHERE relation_types.relation_set_id IN (".$rels_imploded.")";
-        ChromePhp::log($sql);
-		$result = db_fetch_rows($sql);
-		ChromePhp::log("After click");
-		ChromePhp::log($result);
-		return $result;
+            $result = db_fetch_rows($sql);
+            return $result;
+        }
 	}
 	
 }
