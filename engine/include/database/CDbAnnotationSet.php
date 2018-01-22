86bae695e6745c2fafde32019fd224ba1509815d
Mikołaj Szewczyk <mikolaj.m.szewczyk@gmail.com>
2018-01-22 12:22:59 +0100

    #8465 Uproszczenie kwerend w metodzie getCorporaAnnotationSetStats

--- engine/include/database/CDbAnnotationSet.php
+++ engine/include/database/CDbAnnotationSet.php
@@ -33,27 +33,22 @@ class DbAnnotationSet{
     static function getCorporaAnnotationSetStats($annotation_set_id){
         global $db;
 
-        $sql = "SELECT c.id, c.name, c.public, c.description FROM annotation_sets_corpora ansc 
-                JOIN corpora c ON c.id = ansc.corpus_id
-                WHERE ansc.annotation_set_id = ?
-                ORDER BY c.name";
-        $corpora = $db->fetch_rows($sql, array($annotation_set_id));
+        $sql = "SELECT c.id, c.name, c.public, c.description, count(ra.id) AS 'count_uses' FROM annotation_types at
+                JOIN annotation_sets_corpora ascr ON at.group_id = ascr.annotation_set_id
+                JOIN corpora c ON c.id = ascr.corpus_id
+                LEFT JOIN reports_annotations ra ON 
+                (at.name = ra.type AND report_id IN 
+                  (SELECT id FROM reports r WHERE r.corpora = c.id)
+                )
+                WHERE (at.group_id = ? AND ascr.annotation_set_id = ? AND c.public = 1)
+                GROUP BY c.id
+                ORDER BY count_uses";
+        $corpora = $db->fetch_rows($sql, array($annotation_set_id, $annotation_set_id));
 
-        foreach($corpora as $index => $corpus) {
-            $sql = "SELECT count(ra.id) FROM annotation_types at 
-                    LEFT JOIN reports_annotations ra ON 
-                    (at.name = ra.type AND report_id IN 
-                      (SELECT id FROM reports r WHERE r.corpora = ?)
-                    )
-                    WHERE at.group_id = ?";
-            $count_uses = $db->fetch_one($sql, array($corpus['id'], $annotation_set_id));
-
-            $corpora[$index]['count_uses'] = $count_uses;
-        }
         return $corpora;
     }
-	
-	
+
+
 }
 
 ?>
