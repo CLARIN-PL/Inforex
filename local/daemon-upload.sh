1cfff4897e9ea2750aea560ad1be8fc5cef8d98c
Wojciech Rauk <wojtek.rauk@gmail.com>
2018-01-31 16:00:29 +0100

    fixed daemon-upload script

--- local/daemon-upload.sh
+++ local/daemon-upload.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# while true; do
-#  php daemon-upload.php -v
-# done
-
 while true; do
-    /home/wrauk/.phpbrew/php/php-5.3.29/bin/php daemon-upload.php -v
+  php daemon-upload.php -v
 done
