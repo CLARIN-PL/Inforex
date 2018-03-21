4abd202b02f8833e4ea71a5741dd92be99024245
Michał Marcińczuk <marcinczuk@gmail.com>
2018-03-21 13:20:02 +0100

    Fixes to metada fields with multiword names.

--- documentations/sql/update_075_upload.sql
+++ documentations/sql/update_075_upload.sql
@@ -1,1 +1,2 @@
-ALTER TABLE `reports` ADD `filename` TEXT NOT NULL AFTER `lang`;
+ALTER TABLE `reports` ADD `filename` TEXT NULL AFTER `lang`;
+ALTER TABLE `reports` CHANGE `tokenization` `tokenization` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_polish_ci NULL;
--- engine/ajax/a_corpus_edit_ext.php
+++ engine/ajax/a_corpus_edit_ext.php
@@ -26,15 +26,14 @@ class Ajax_corpus_edit_ext extends CPage {
         $enum_values = $_POST['enum_values'];
         $default = ($_POST['default'] == "null" || $_POST['default'] == "" ? null : $_POST['default']);
 
-        ChromePhp::log("corpus_edit_ext");
-        ChromePhp::log($_POST);
-
 		if ($action == 'get'){
+		    // ToDO: Change to DB method
 			$sql = "SELECT ext FROM corpora WHERE id=?";
 			$ext = $db->fetch_one($sql, array($corpus['id']));
 			return DbCorpus::getCorpusExtColumns($ext);
 		}			
 		elseif ($action == 'add'){
+            // ToDO: Change to DB method
 			$sql = "SELECT ext FROM corpora WHERE id=?";
 			$ext = $db->fetch_one($sql, array($corpus['id']));
 
@@ -42,22 +41,27 @@ class Ajax_corpus_edit_ext extends CPage {
                 $ext = "reports_ext_" . $corpus['id'];
 
                 if($type == "enum"){
-                    $sql = "CREATE TABLE IF NOT EXISTS `".$ext."` (`id` BIGINT(20) AUTO_INCREMENT PRIMARY KEY ,`".$name."` ".$type."(".$enum_values.") ". ($default == null ? "" : " DEFAULT '".$default."' NOT" ) . " NULL COMMENT '".$field_name . "###" . $comment."')";
+                    // ToDO: Change to DB method
+                    $sqlComment = sprintf("%s###%s", $field_name, $comment);
+                    $sql = "CREATE TABLE IF NOT EXISTS `".$ext."` (`id` BIGINT(20) AUTO_INCREMENT PRIMARY KEY ,`".$name."` ".$type."(".$enum_values.") ". ($default == null ? "" : " DEFAULT '".$default."' NOT" ) . " NULL COMMENT '$sqlComment')";
                 } else{
-                    $sql = "CREATE TABLE IF NOT EXISTS `".$ext."` (`id` BIGINT(20) AUTO_INCREMENT PRIMARY KEY ,`".$name."` ".$type." ". ($default == null ? "" : " NOT" ) . " NULL COMMENT '".$field_name . "###" . $comment . ($default == null ? "" : "###" . $default )."'";
+                    // ToDO: Change to DB method
+                    $sqlComment = sprintf("%s###%s", $field_name, $comment . ($default == null ? "" : "###$default"));
+                    $sql = "CREATE TABLE IF NOT EXISTS `".$ext."` (`id` BIGINT(20) AUTO_INCREMENT PRIMARY KEY ,`".$name."` ".$type." ". ($default == null ? "" : " NOT" ) . " NULL COMMENT '$sqlComment')";
                 }
-                ChromePhp::log($sql);
 			    $db->execute($sql);
 
+                // ToDO: Change to DB method
                 $sql = "UPDATE corpora SET ext = ? WHERE id = ?";
                 $db->execute($sql, array($ext, $corpus['id']));
-            } else{
+            } else {
 			    if($type == "enum"){
+                    // ToDO: Change to DB method
                     $sql = "ALTER TABLE {$ext} ADD {$_POST['field']} {$_POST['type']}({$enum_values}) ". ($default == null ? "" : " DEFAULT '".$default."' NOT" ) . " NULL COMMENT '".$field_name."###".$comment."'";
                 } else{
+                    // ToDO: Change to DB method
                     $sql = "ALTER TABLE {$ext} ADD {$_POST['field']} {$_POST['type']} ". ($default == null ? "" : " NOT" ) . " NULL COMMENT '".$field_name."###".$comment . ($default == null ? "" : "###" . $default )."'";
                 }
-                ChromePhp::log($sql);
                 ob_start();
                 $db->execute($sql);
                 $error_buffer_content = ob_get_contents();
@@ -69,43 +73,50 @@ class Ajax_corpus_edit_ext extends CPage {
             }
 		}
 		elseif ($action == 'edit'){
-			ChromePhp::log("Editing");
+            // ToDO: Change to DB method
 			$sql = "SELECT ext FROM corpora WHERE id=?";
 			$ext = $db->fetch_one($sql, array($corpus['id']));
 
             if($type == "enum"){
+                // ToDO: Change to DB method
                 $sql = "ALTER TABLE {$ext} CHANGE {$_POST['old_field']} {$name} {$type}({$enum_values}) ". ($default == null ? "" : " DEFAULT '".$default."' NOT" ) . " NULL COMMENT '".$field_name . "###" . $comment."'";
             } else{
+                // ToDO: Change to DB method
                 $sql = "ALTER TABLE {$ext} CHANGE {$_POST['old_field']} {$name} {$type} ". ($default == null ? "" : " NOT" ) . " NULL COMMENT '".$field_name . "###" . $comment . ($default == null ? "" : "###" . $default )."'";
             }
 
             ob_start();
-            ChromePhp::log($sql);
 			$db->execute($sql);
 			$error_buffer_content = ob_get_contents();
 			ob_clean();
-			if(strlen($error_buffer_content))
-				throw new Exception("Error: ". $error_buffer_content);
-			else
-				return;
+			if(strlen($error_buffer_content)) {
+                throw new Exception("Error: " . $error_buffer_content);
+            }else {
+                return;
+            }
 		}
 		else if($action == 'delete'){
+            // ToDO: Change to DB method
             $sql = "SELECT ext FROM corpora WHERE id=?";
             $table_name = $db->fetch_one($sql, array($corpus['id']));
 
             if(count(DbCorpus::getCorpusExtColumns($table_name)) > 1){
+                // ToDO: Change to DB method
                 $sql = "ALTER TABLE ".$table_name." DROP " .$name;
                 $db->execute($sql);
             } else{
+                // ToDO: Change to DB method
                 $sql = "DROP TABLE ".$table_name;
                 $db->execute($sql);
 
+                // ToDO: Change to DB method
                 $sql = "UPDATE corpora SET ext = '' WHERE id = ?";
                 $db->execute($sql, $corpus['id']);
             }
         }
 		elseif ($action == 'add_table'){
 			$table_name = "reports_ext_".$corpus['id'];
+            // ToDO: Change to DB method
 			$sql = "CREATE TABLE {$table_name} (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY , {$_POST['field']} {$_POST['type']} ". ($_POST['is_null'] == "true" ? "" : " NOT" ) . " NULL ) ENGINE = InnoDB ";
 			ob_start();
 			$db->execute($sql);
@@ -114,15 +125,17 @@ class Ajax_corpus_edit_ext extends CPage {
 			if(strlen($error_buffer_content))
 				throw new Exception("Error: ". $error_buffer_content);
 			else{
+                // ToDO: Change to DB method
 				$sql = "UPDATE corpora SET ext = '{$table_name}' WHERE id = {$corpus['id']}";
 				ob_start();
 				$db->execute($sql);
 				$error_buffer_content = ob_get_contents();
 				ob_clean();
-				if(strlen($error_buffer_content))
-					throw new Exception("Error: ". $error_buffer_content);
-				else
-					return;
+				if(strlen($error_buffer_content)) {
+                    throw new Exception("Error: " . $error_buffer_content);
+                }else {
+                    return;
+                }
 			}			
 		}
 		else{
