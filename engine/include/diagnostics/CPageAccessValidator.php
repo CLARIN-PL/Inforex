ee61f02a8014a35379f8553041c2f0754e544fa1
Michał Marcińczuk <marcinczuk@gmail.com>
2018-04-16 13:21:11 +0200

    Resolve config problem

--- engine/include/diagnostics/CPageAccessValidator.php
+++ engine/include/diagnostics/CPageAccessValidator.php
@@ -20,7 +20,7 @@ class PageAccessValidator{
     }
 
     function analyze($ajaxFilename){
-        //global $config;
+        global $config;
         $path = $this->type . DIRECTORY_SEPARATOR . $ajaxFilename;
         require_once($path);
         $className = ucfirst(substr($ajaxFilename, 0, strlen($ajaxFilename)-4));
