2a8f8b87523a98be8d27cc4e6150ac386280cbaf
Michał Marcińczuk <marcinczuk@gmail.com>
2018-05-10 16:24:39 +0200

    Removed unused code

--- public_html/js/page_stats.js
+++ public_html/js/page_stats.js
@@ -1,23 +1,12 @@
 $(function(){
     var url = $.url(window.location.href);
     var corpus_id = url.param('corpus');
-    vars = [];
-    var hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
 
     $(".corpus_flag_id, .flag_type").change(function(){
         var flag_val = $(".corpus_flag_id").val();
         var flag_status = $(".flag_type").val();
         var status = $(".selected_status_id").attr('id');
-
-        console.log(status);
-
+        
         if(flag_status !== "-" && flag_val !== "-"){
             window.location.href = "index.php?page=stats&corpus="+corpus_id+"&status="+status+"&flag="+flag_val+"&flag_status="+flag_status;
         }
