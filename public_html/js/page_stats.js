29867202c8ab18c85a70cb63f86b32bc295f894c
Mikolaj Szewczyk <mikolaj.m.szewczyk@gmail.com>
2018-05-10 13:51:08 +0200

    #8635 Naprawa strony ze statystykami tokenow i slow

--- public_html/js/page_stats.js
+++ public_html/js/page_stats.js
@@ -14,7 +14,9 @@ $(function(){
     $(".corpus_flag_id, .flag_type").change(function(){
         var flag_val = $(".corpus_flag_id").val();
         var flag_status = $(".flag_type").val();
-        var status = $(".selected_status").attr('id');
+        var status = $(".selected_status_id").attr('id');
+
+        console.log(status);
 
         if(flag_status !== "-" && flag_val !== "-"){
             window.location.href = "index.php?page=stats&corpus="+corpus_id+"&status="+status+"&flag="+flag_val+"&flag_status="+flag_status;
