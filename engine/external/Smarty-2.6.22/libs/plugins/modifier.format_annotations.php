c8edd7f4a5334c9f9372c2d24495f67a8f5beff2
Michał Marcińczuk <marcinczuk@gmail.com>
2018-10-02 14:43:12 +0200

    Sentence fix

--- engine/external/Smarty-2.6.22/libs/plugins/modifier.format_annotations.php
+++ engine/external/Smarty-2.6.22/libs/plugins/modifier.format_annotations.php
@@ -9,6 +9,7 @@ function smarty_modifier_format_annotations($string)
 	$string = preg_replace('/<an#(\d+):([^:]+):(\d+):(\d+)>/u', "<span id='an$1' class='ann annotation_set_$3 $2' groupid='$3' subgroupid='$4' title='an#$1:$2'>", $string);
 	$string = preg_replace('/<an#(\d+):([^:]+):(\d+)>/u', "<span id='an$1' class='ann annotation_set_$3 $2' groupid='$3' title='an#$1:$2'>", $string);
 	$string = preg_replace('/<an#(\d+):([a-z0-9_]+)>/', "<span id='an$1' class='$2' title='an#$1:$2'>", $string);
+    $string = preg_replace('/<an#(\d+):([a-z0-9_]+) eos>/', "<span id='an$1' class='$2 eos' title='an#$1:$2'>", $string);
 	$string = str_replace("</an>", "</span>", $string);
 	return $string;
 }
