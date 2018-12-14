33e6fc594ed8ae19b3cf2673b8187b3feccfb456
Michał Marcińczuk <marcinczuk@gmail.com>
2018-12-14 11:38:33 +0100

    Cascade on delete for flag history

---
+++ documentations/sql/update_082_cascade_flag_history.sql
@@ -0,0 +1,4 @@
+ALTER TABLE  `flag_status_history` DROP FOREIGN KEY  `flag_status_history_ibfk_1` ,
+ADD FOREIGN KEY (  `report_id` ) REFERENCES  `reports` (
+`id`
+) ON DELETE CASCADE ON UPDATE RESTRICT ;
