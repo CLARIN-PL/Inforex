301b96cf7eac12be3d6a2d3d8b44e6369eb2777d
Michał Marcińczuk <marcinczuk@gmail.com>
2019-06-04 12:51:44 +0200

    Sql database update

--- database/inforex-v1.0-changelog.sql
+++ database/inforex-v1.0-changelog.sql
@@ -66,3 +66,82 @@ INSERT INTO `corpus_roles` (`role`, `description`, `description_long`) VALUES
 ('tasks', 'Tasks', ''),
 ('wccl_match', 'Wccl Match', '');
 
+
+--changeset czuk:4
+
+ALTER TABLE `reports_users_selection` ADD INDEX(`user_id`);
+
+ALTER TABLE `reports_users_selection` ADD FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `reports_users_selection` ADD INDEX(`report_id`);
+
+ALTER TABLE `reports_users_selection` ADD  FOREIGN KEY (`report_id`) REFERENCES `reports`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `reports_users_selection` ADD UNIQUE( `user_id`, `report_id`);
+
+
+--changeset czuk:5
+
+INSERT INTO `report_perspectives` (`id`, `title`, `description`, `order`) VALUES ('annotation_table', 'Annotation table', 'Display a summary of annotations', '100');
+
+
+--changeset czuk:6
+
+INSERT INTO `tagsets` (`tagset_id`, `name`) VALUES ('4', 'Hebrew UDPipe'), ('5', 'Russian UDPipe');
+INSERT INTO `tagsets` (`tagset_id`, `name`) VALUES ('6', 'Czech UDPipe'), ('7', 'Bulgarian UDPipe');
+
+
+--changeset czuk:7
+
+--
+-- Struktura tabeli dla tabeli `orths`
+--
+
+CREATE TABLE `orths` (
+  `orth_id` bigint(20) NOT NULL,
+  `orth` varchar(190) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `orths`
+--
+ALTER TABLE `orths`
+  ADD PRIMARY KEY (`orth_id`),
+  ADD KEY `orth` (`orth`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `orths`
+--
+ALTER TABLE `orths`
+  MODIFY `orth_id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `orths` ADD UNIQUE(`orth`);
+
+ALTER TABLE `tokens` ADD `orth_id` BIGINT NULL AFTER `eos`;
+
+
+--changeset czuk:8
+
+ALTER TABLE `reports_annotations_shared_attributes` DROP FOREIGN KEY `reports_annotations_shared_attributes_ibfk_4`;
+
+ALTER TABLE `reports_annotations_shared_attributes` CHANGE `value` `value` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+
+ALTER TABLE `shared_attributes_enum` CHANGE `value` `value` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+
+ALTER TABLE `shared_attributes_enum` CHANGE `description` `description` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
+
+ALTER TABLE `reports_annotations_shared_attributes`
+  ADD CONSTRAINT `reports_annotations_shared_attributes_ibfk_4` FOREIGN KEY (`value`) REFERENCES `annotation_types_attributes_enum` (`value`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+
+--changeset czuk:9
+
+INSERT INTO `report_perspectives` (`id`, `title`, `description`, `order`) VALUES ('annotation_attributes', 'Annotation attributes', 'Batch editor of annotation attributes', '110');
