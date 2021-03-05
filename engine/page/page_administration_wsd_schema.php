c2aa49f4255815a3bc75f1acfe541017e4dfd634
Tomasz Naskręt <tomasz.naskret@pwr.edu.pl>
2021-03-05 11:44:34 +0100

    post merge cleanup

--- engine/page/page_administration_wsd_schema.php
+++ engine/page/page_administration_wsd_schema.php
@@ -10,9 +10,8 @@ class Page_administration_wsd_schema extends CPageAdministration {
 
 	function execute(){
 		$sens = DbSens::getSenseList();
-        var_dump($sens);
+
 		foreach($sens as $key => $value){
-            var_dump($key);
 			$sens[$key]['annotation_name'] = substr($sens[$key]['annotation_name'], 4);
 		}
 		$this->set("sensList", $sens);
