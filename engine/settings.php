2b61f7ec4c22d971c1195a6131a10bb4cbe467db
Seweryn Walentynowicz <seweryn@walor.torun.pl>
2021-03-09 18:13:04 +0100

    test environment to two constructors problem

--- engine/settings.php
+++ engine/settings.php
@@ -16,7 +16,13 @@ ini_set("output_buffering", 0);
 ini_set("short_open_tag",1);
 setlocale(LC_CTYPE, "en_US.UTF-8");		
 
+// to psuje podwójne konstruktory
 error_reporting(E_ALL);
+// a dowolny z poniższych blokuje te komunikaty
+//error_reporting(E_ALL ^ E_STRICT);
+ini_set("error_reporting", E_ALL & ~E_STRICT );
+// mój handler poniżej nie jest wrażliwy na ustawienia error_reporting
+// i wywala wszystko zawsze:
 
 function inforexCentralErrorHandler($level, $message, $file = ’’, $line = 0) {
 	print("[".$level."] ".$message." in ".$file.":".$line."<br/>\n");
