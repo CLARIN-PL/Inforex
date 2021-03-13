25c04428fca7f222c9f8352ea5783141ff5c3f0e
Seweryn Walentynowicz <S.Walentynowicz@walor.torun.pl>
2021-03-13 11:29:17 +0100

    docker local config file upgraded

--- docker/www/sample.config.local.php
+++ docker/www/sample.config.local.php
@@ -3,7 +3,7 @@ Config::Config()->put_path_engine('/home/inforex/engine');
 Config::Config()->put_path_www('/home/inforex/public_html');
 Config::Config()->put_path_secured_data('/home/inforex/secured_data');
 Config::Config()->put_dsn(array(
-    'phptype'  => 'mysql',
+    'phptype'  => 'mysqli',
     'username' => 'inforex', 'port' => '3306', 'password' => 'password',
     'hostspec' => 'db',
     'database' => 'inforex',
