d421e1d5b8ff6005982304d9482decf8ffd5b27b
Tomasz Naskręt <tomasz.naskret@pwr.edu.pl>
2021-08-16 12:22:44 +0200

    fix lang display on metadata view

--- engine/page/report_perspectives/PerspectiveMetadata.php
+++ engine/page/report_perspectives/PerspectiveMetadata.php
@@ -23,11 +23,10 @@ class PerspectiveMetadata extends CPerspective {
     function execute(){
 
         global $corpus;
-		$row = $this->page->get("row");
         $report = $this->page->report;
 
-		if($row['parent_report_id'] !== null){
-            $parent_report = DbReport::getParentReport($row['parent_report_id']);
+		if($report['parent_report_id'] !== null){
+            $parent_report = DbReport::getParentReport($report['parent_report_id']);
             $this->page->set("parent_report", $parent_report);
         }
 		$ext = DbReport::getReportExtById($report["id"]);
@@ -35,14 +34,14 @@ class PerspectiveMetadata extends CPerspective {
 		$subcorpora = DbCorpus::getCorpusSubcorpora($corpus['id']);
 		$statuses = DbStatus::getAll();
 		$formats = DbReport::getAllFormats();
-		$language = DbReport::getFullLanguageName($row['lang']);
+		$language = DbReport::getFullLanguageName($report['lang']);
         $content_with_format = DBReport::getReportContentAndFormatById($report['id']);
 		$this->page->set("report_language", $language);
 
 		/* Jeżeli nie ma rozszrzonego wiersza atrybutów, to utwórz pusty */
 		if ( $ext == null && $corpus['ext'] != "" ){
-			DbReport::insertEmptyReportExt($row['id']);
-			$ext = DbReport::getReportExtById($row['id']);
+			DbReport::insertEmptyReportExt($report['id']);
+			$ext = DbReport::getReportExtById($report['id']);
 		}
 		
 		$features_index = array();
@@ -73,6 +72,7 @@ class PerspectiveMetadata extends CPerspective {
 
 		$this->page->set("content", $content);
 		$this->page->set("features", $features);
+        $this->page->set("features", $features);
 		$this->page->set("subcorpora", $subcorpora);
 		$this->page->set("statuses", $statuses);	
 		$this->page->set("formats", $formats);
