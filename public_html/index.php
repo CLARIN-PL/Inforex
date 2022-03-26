bd540671bd795f318af7365365a88b2f4faf5c06
Seweryn Walentynowicz <Seweryn.Walentynowicz@frakoterm.pl>
2022-03-26 13:14:53 +0100

    main catch service should be smarter

--- public_html/index.php
+++ public_html/index.php
@@ -12,6 +12,7 @@ try{
 	$enginePath = realpath(__DIR__ . "/../engine/");
 	require_once($enginePath."/settings.php");
 	require_once($enginePath.'/include.php');
+    
 	Config::Config()->put_path_engine($enginePath);
 	Config::Config()->put_localConfigFilename(realpath($enginePath."/../config/").DIRECTORY_SEPARATOR."config.local.php");
 
@@ -65,7 +66,27 @@ try{
 	print trim(ob_get_clean());
 }
 catch(Exception $e){
-	print "Unexpected exception: <b>" . $e->getMessage() . "</b>";
-	print "<pre>".$e->getTraceAsString()."</pre>";
-    print trim(ob_get_clean());
+	//print "Unexpected exception: <b>" . $e->getMessage() . "</b>";
+	//print "<pre>".$e->getTraceAsString()."</pre>";
+    //print trim(ob_get_clean());
+
+    $NL=''; $BOpen=''; $BClose=''; $PreOpen=''; $PreClose='';
+    if (php_sapi_name() !== "cli") {
+        $NL = '<br/>'; $BOpen='<b>'; $BClose='</b>'; $PreOpen='<pre>'; $PreClose='<pre/>';
+    }
+    $NL .= "\n";
+    if(ini_get('display_errors')){
+        print("Uncaught exception: ".$BOpen. $e->getMessage().$BClose.$NL);
+        print($PreOpen." in file ".$e->getFile()." on line ".$e->getLine().$PreClose.$NL);
+        print($PreOpen.$e->getTraceAsString().$PreClose.$NL);
+    } else {
+        print($BOpen."Uncaught exception has been occurred.".$BClose.$NL);
+        print("Please try again later.".$NL);
+    }
+    // komunikat o błędzie do logu - zawsze, chyba że zablokujemy w ogóle
+    if(ini_get('log_errors')){
+        //error_log("Uncaught exception: ".print_r($e,true));
+        error_log("Uncaught exception: ".$e->getMessage()." in file ".$e->getFile()." on line ".$e->getLine()." Trace:".$e->getTraceAsString());
+    }
+
 }
