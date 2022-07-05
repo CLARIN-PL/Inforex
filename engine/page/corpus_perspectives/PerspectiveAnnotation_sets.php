0ff05bf67dc05dc75b121b76e85d2b7721f581f5
Seweryn Walentynowicz <Seweryn.Walentynowicz@frakoterm.pl>
2022-07-05 12:04:43 +0200

    restored original sql query from rev. 81a95ce3

--- engine/page/corpus_perspectives/PerspectiveAnnotation_sets.php
+++ engine/page/corpus_perspectives/PerspectiveAnnotation_sets.php
@@ -12,20 +12,7 @@ class PerspectiveAnnotation_sets extends CCorpusPerspective {
 	{
 		global $corpus, $db;
 
-		$sql = "SELECT t.id, t.name, t.cid, t.count_ann FROM (" .
-			" SELECT ase.annotation_set_id AS id, ase.name as name,asco.corpus_id AS cid, count(ra.id) as count_ann" .
-			" FROM annotation_sets ase" .
-			" LEFT JOIN annotation_sets_corpora asco ON (ase.annotation_set_id = asco.annotation_set_id)" .
-			" LEFT JOIN reports_annotations ra ON (ase.annotation_set_id = ra.group AND ra.report_id IN (SELECT id FROM reports r WHERE r.corpora = ?))" .
-			" WHERE  asco.corpus_id = ?" .
-			" GROUP BY ase.annotation_set_id" .
-			" UNION ALL" .
-			" SELECT ase.annotation_set_id as id, ase.name as name, null as cid, 0 as count_ann" .
-			" FROM annotation_sets ase" .
-			" LEFT JOIN annotation_sets_corpora asco ON ase.annotation_set_id = asco.annotation_set_id" .
-			" WHERE ase.annotation_set_id IS NOT NULL" .
-			" GROUP BY ase.name) t" .
-			" GROUP BY t.name ORDER BY t.id";
+		$sql = "SELECT ase.annotation_set_id AS id, ase.name, asco.corpus_id AS cid, count(ra.id) as count_ann FROM annotation_sets ase LEFT JOIN annotation_sets_corpora asco ON (ase.annotation_set_id = asco.annotation_set_id AND asco.corpus_id = ?) LEFT JOIN annotation_types at ON (ase.annotation_set_id = at.group_id) LEFT JOIN reports_annotations ra ON (at.name = ra.type AND ra.report_id IN (SELECT id FROM reports r WHERE r.corpora = ?)) GROUP BY ase.annotation_set_id";
 
 		$this->page->set('annotationsList', $db->fetch_rows($sql, array($corpus['id'], $corpus['id'])));
 	}
