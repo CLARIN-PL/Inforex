f209e0fec793881450f2872dd73c52ede82c6af7
Seweryn Walentynowicz <Seweryn.Walentynowicz@walor.torun.pl>
2023-10-21 18:25:50 +0200

    tests for lemmas and anntags exports

--- phpunit/tests/engine/include/export/ConllAndJsonFactoryTest.php
+++ phpunit/tests/engine/include/export/ConllAndJsonFactoryTest.php
@@ -97,6 +97,18 @@ class ConllAndJsonFactoryTest extends PHPUnit_Framework_TestCase {
 		return $expectedConll; 
     } // getExpectedConll()
 
+    private function extractAnntagsFromConllLines($conllStr,$lineNo) {
+
+        // divide to lines
+        $conllLines = explode("\n",$conllStr);
+        // line should looks like "2\t2\tduże\t\t6\t9\tO\t_\t_\t_"
+        $connLineForToken = $conllLines[$lineNo];
+        $columns = explode("\t",$connLineForToken);
+        $annTags = $columns[6];
+        return $annTags;
+
+    } // extractAnntagsFromConllLines()
+
 	private function getExpectedEmptyJson() {
 
         return 	array(
@@ -133,9 +145,22 @@ class ConllAndJsonFactoryTest extends PHPUnit_Framework_TestCase {
 
     private function generateAnnotation_By_IdTestData(
                         $annotationExtractorFields=true,
+						$annotation_idExtractorFields=true,
                         $annotationsWithLemmaField=true,
                         $annotationsWithTypeAttributte=true
                     ) {
+
+		// Depends from extractor type, annotations row have different
+		// fields:
+		//  Common section: 
+		//		'id','report_id','type_id','from','to','text','user_id','creation_time','stage','source','annotation_subset_id','type','group_id'
+		//  Fields, that exists only for 'annotation' extractor: 
+		//		'login','screename','lemma'( exists always, but may be null if no lemma )
+		//  Fields that exist for extractors 'annotation_id' or 'annotation_subset_id' : 
+		//		'group','name','description','css','cross_sentence','shortlist'
+		//  Fields that exists only if type attribute is set : 
+		//		'annotation_id','annotation_attribute_id','value','prop'
+		
 
         $annotation_id = 1;
         // index must match 'id' field
@@ -153,13 +178,9 @@ class ConllAndJsonFactoryTest extends PHPUnit_Framework_TestCase {
             // fields, that exists for extractor 'annotation' only
             $annotations_by_id[$annotation_id]['login']='admin';
             $annotations_by_id[$annotation_id]['screename']='Inforex Admin';
-            if($annotationsWithLemmaField){
-                $annotations_by_id[$annotation_id]['lemma']='lemat dodany do duże';
-            } else {
-                $annotations_by_id[$annotation_id]['lemma']=null;
-            }
-        } else {
-            // fields, that exists for extractors 'annotation_id' 
+		}
+		if($annotation_idExtractorFields) {
+            // fields, that exists for extractors 'annotation_id'
             //   and 'annotation_subset_id' only
             $annotations_by_id[$annotation_id]['group']=1;
             $annotations_by_id[$annotation_id]['name']='nam_adj';
@@ -167,14 +188,18 @@ class ConllAndJsonFactoryTest extends PHPUnit_Framework_TestCase {
             $annotations_by_id[$annotation_id]['css']='background: lightgreen;';
             $annotations_by_id[$annotation_id]['cross_sentence']='0';
             $annotations_by_id[$annotation_id]['shortlist']='0';
-            // this section only for simple extractor with attribute in DB
-            if($annotationsWithTypeAttributte) {
-                $annotations_by_id[$annotation_id]['annotation_id']=$annotation_id;
-                $annotations_by_id[$annotation_id]['annotation_attribute_id']=1;
-                $annotations_by_id[$annotation_id]['value']='valueAtrTypu';
-                $annotations_by_id[$annotation_id]['prop']='valueAtrTypu';
-            }
-        }
+		}
+     	if($annotationsWithLemmaField){
+         	$annotations_by_id[$annotation_id]['lemma']='lemat dodany do duże';
+      	} else {
+          	$annotations_by_id[$annotation_id]['lemma']=null;
+       	}
+    	if($annotationsWithTypeAttributte) {
+          	$annotations_by_id[$annotation_id]['annotation_id']=$annotation_id;
+           	$annotations_by_id[$annotation_id]['annotation_attribute_id']=1;
+           	$annotations_by_id[$annotation_id]['value']='valueAtrTypu';
+           	$annotations_by_id[$annotation_id]['prop']='valueAtrTypu';
+       	}
 
         return $annotations_by_id;
 
@@ -184,6 +209,23 @@ class ConllAndJsonFactoryTest extends PHPUnit_Framework_TestCase {
         return array_values($annotations_by_id);
 
     } // generateAnnotationsFromAnnotations_By_Id
+
+    private function generateRelationsTestData() {
+
+        $relationData = array(
+            'report_id' => '1',
+            'id' => '1',
+            'relation_type_id' => '1',
+            'source_id' => '1',
+            'target_id' => '2',
+            'relation_set_id' => '1',
+            'name' => 'test',
+            'rsname' => 'test',
+        );
+        $relations = array( $relationData );
+        return $relations;
+
+    } // generateRelationsTestData()
 
     private function generateFullTestData() {
 
@@ -377,20 +419,9 @@ class ConllAndJsonFactoryTest extends PHPUnit_Framework_TestCase {
     public function testAllDataPlacesToDataConllAndJsonStructures() {
 
         // args for call
-        $relations = array();
-		$relationData = array(
-    		'report_id' => '1',
-    		'id' => '1',
-    		'relation_type_id' => '1',
-    		'source_id' => '1',
-    		'target_id' => '2',
-    		'relation_set_id' => '1',
-    		'name' => 'test',
-    		'rsname' => 'test',
-        );
-		$relations = array( $relationData );
-        $annotations_by_id = $this->generateAnnotation_By_IdTestData(False,True);
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData(False,True,False,True);
         $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        $relations = $this->generateRelationsTestData();
 
 		// invoke tested method
         list($conll,$json_builder) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
@@ -409,23 +440,21 @@ class ConllAndJsonFactoryTest extends PHPUnit_Framework_TestCase {
 		$expectedChunks[0][0][2]['relations'][0] = 1;
         $expectedJson = array(
             "chunks" => $expectedChunks,
-            "relations" => array(
-				$relationData
-			),
+            "relations" => $relations,
             "annotations" => array(
                 array('id'=>1,'report_id'=>1,'type_id'=>360,'from'=>6,'to'=>9,
                     'text' => 'duże','user_id' => 1,
                     'creation_time' => '2022-10-03 08:07:37','stage' => 'final',
                     'source' => 'user','annotation_subset_id' => '52',
                     'type'=>'nam_adj','group_id'=>1,
-/*
-                    'login' => 'admin','screename' => 'Inforex Admin',
-                    'lemma' => 'lemat dodany do duże',
-*/
+                    //'login' => 'admin','screename' => 'Inforex Admin',
+                    //'lemma' => 'lemat dodany do duże',
                     'group' => 1,'description' => 'Przymiotnik utworzony od nazwy własnej','css' => 'background: lightgreen;','cross_sentence' => '0','shortlist' => '0',
                     'name'=>'nam_adj', 
                     // tylko jeśli istnieje atrybut
-                    'annotation_id' => 1,'annotation_attribute_id' => 1,'value' => 'valueAtrTypu','prop' => 'valueAtrTypu'
+                    'annotation_id' => 1,'annotation_attribute_id' => 1,'value' => 'valueAtrTypu','prop' => 'valueAtrTypu',
+                    // puste lemma
+                    'lemma' => null
                 ),
             )
         );
@@ -466,11 +495,194 @@ class ConllAndJsonFactoryTest extends PHPUnit_Framework_TestCase {
     public function testGeneralAnnotationFieldsPlacedToJsonAndConllArray() {
 
         // are all fields which always should be present in annotation 
-        // record placed correctly to JSON and CONLL structures
+        // record placed correctly to JSON and CONLL structures ?
+        // args for call without lemma
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData(); // default annotation with: all extractors fields, lemma & type attribute
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        $relations = $this->generateRelationsTestData();
 
-        $this->assertTrue(True);
+        // invoke tested method
+        list($conll,$json_builder) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+
+        // check results
+        $expectedConll = $this->getExpectedConll();
+        $expectedConll = str_replace(
+                            "2\t2\tduże\t\t6\t9\tO\t_\t_\t_\n",
+                            "2\t2\tduże\t\t6\t9\tB-nam_adj\t1\t1\t2\n",
+                            $expectedConll);
+        $this->assertEquals($expectedConll,$conll);
+
+        $expectedChunks = $this->getExpectedChunks();
+        // annotation_id to 'annotations' list in 3-rd chunk
+        $expectedChunks[0][0][2]['annotations'][] = 1;
+        $expectedChunks[0][0][2]['relations'][0] = 1;
+        $expectedJson = array(
+            "chunks" => $expectedChunks,
+            "relations" => $relations,
+            "annotations" => array(
+                array('id'=>1,'report_id'=>1,'type_id'=>360,'from'=>6,'to'=>9,
+                    'text' => 'duże','user_id' => 1,
+                    'creation_time' => '2022-10-03 08:07:37','stage' => 'final',
+                    'source' => 'user','annotation_subset_id' => '52',
+                    'type'=>'nam_adj','group_id'=>1,
+					// 'annotation' extractor group
+                    'login' => 'admin','screename' => 'Inforex Admin',
+                    'lemma' => 'lemat dodany do duże',
+					// end of 'annotation' extractor group
+// these fields shows only for annotationExtractor = false
+                    'group' => 1,'description' => 'Przymiotnik utworzony od nazwy własnej','css' => 'background: lightgreen;','cross_sentence' => '0','shortlist' => '0',
+                    'name'=>'nam_adj',
+                    // tylko jeśli istnieje atrybut
+                    'annotation_id' => 1,'annotation_attribute_id' => 1,'value' => 'valueAtrTypu','prop' => 'valueAtrTypu'
+                ),
+            )
+        );
+		$this->assertEquals($expectedJson,$json_builder); 
 
     } // testGeneralAnnotationFieldsPlacedToJsonAndConllArray
+
+    public function testLemmaAdsNotChangesConll() {
+
+        $relations = $this->generateRelationsTestData();
+        // args for call without lemma
+		$LemmaExists = False;
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData(True,True,$LemmaExists);
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        // invoke tested method
+        list($conll,$json_builder) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+
+        // args for call with lemma
+        $LemmaExists = True;
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData(True,True,$LemmaExists);
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        // invoke tested method
+        list($conll_wL,$json_builder_wL) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+
+		// conll not changed after add lemma:
+		$this->assertEquals($conll,$conll_wL);
+
+    } // testLemmaAdsNotChangesConll() 
+
+    public function testLemmaContentChangesNullInLemmaFieldInJson() {
+
+        $relations = $this->generateRelationsTestData();
+        // args for call without lemma
+        $LemmaExists = False;
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData(True,False,$LemmaExists);
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        // invoke tested method
+        list($conll,$json_builder) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+
+        // args for call with lemma
+        $LemmaExists = True;
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData(True,False,$LemmaExists);
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        // invoke tested method
+        list($conll_wL,$json_builder_wL) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+
+        // json results after add lemma:
+		//   field 'lemma' changes from null to lemma text
+        $expectedJsonWithLemma = $json_builder;
+ 		$expectedJsonWithLemma['annotations'][0]['lemma'] = $annotations[0]['lemma'];
+        $this->assertEquals($expectedJsonWithLemma,$json_builder_wL);
+
+    } // testLemmaContentChangesNullInLemmaFieldInJson()
+
+    public function testTokensWithoutAnnotationHasOAnntag() {
+
+		// args for call w/o annotations
+        $relations = array();
+        $annotations_by_id = array();
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        // invoke tested method
+        list($conll,$json_builder) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+		
+        $expectedAnnTags = "O";
+		$fromLine = 3;
+		$annTags = $this->extractAnntagsFromConllLines($conll,$fromLine); 
+		$this->assertEquals($expectedAnnTags,$annTags);
+
+    } // testTokensWithoutAnnotationHasOAnntag()
+
+    public function testFirstTokenIntoAnnotationHasBAnntag() {
+
+		$annotationTokenLine = 3;
+		$relations = array();
+        // call args for annotations without "name" field
+  		$fieldsForAnnotationIdExtractorExists = False;
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData(True,$fieldsForAnnotationIdExtractorExists);
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        // invoke tested method
+        list($conll,$json_builder) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+
+        $expectedAnnTags = "B-";
+        $annTags = $this->extractAnntagsFromConllLines($conll,$annotationTokenLine);
+        $this->assertEquals($expectedAnnTags,$annTags);
+
+        // call args for annotations with "name" field
+        $fieldsForAnnotationIdExtractorExists = True;
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData(True,$fieldsForAnnotationIdExtractorExists);
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        // invoke tested method
+        list($conll,$json_builder) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+
+        $expectedAnnTags = "B-".'nam_adj'; // annotation type glued
+        $annTags = $this->extractAnntagsFromConllLines($conll,$annotationTokenLine);
+        $this->assertEquals($expectedAnnTags,$annTags);
+
+    } // testFirstTokenIntoAnnotationHasBAnntag()
+
+	public function testNextTokensIntoAnnotationHasIAnntag() {
+
+        $relations = array();
+        // call args for annotations without "name" field
+        $fieldsForAnnotationIdExtractorExists = False;
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData(True,$fieldsForAnnotationIdExtractorExists);
+		// expand annotation 1 to charpos 20 includes 2 next tokens
+		$annotations_by_id[1]['to']=14;
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        // invoke tested method
+        list($conll,$json_builder) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+
+		$expectedAnnTags = "I-";
+		// for tokens in lines 4 and 5
+        $this->assertEquals($expectedAnnTags,$this->extractAnntagsFromConllLines($conll,4));
+		$this->assertEquals($expectedAnnTags,$this->extractAnntagsFromConllLines($conll,5));
+		
+        // call args for annotations with "name" field
+        $fieldsForAnnotationIdExtractorExists = True;
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData(True,$fieldsForAnnotationIdExtractorExists);
+        // expand annotation 1 to charpos 20 includes 2 next tokens
+        $annotations_by_id[1]['to']=14;
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        // invoke tested method
+        list($conll,$json_builder) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+
+        $expectedAnnTags = "I-nam_adj";
+        // for tokens in lines 4 and 5
+        $this->assertEquals($expectedAnnTags,$this->extractAnntagsFromConllLines($conll,4));
+        $this->assertEquals($expectedAnnTags,$this->extractAnntagsFromConllLines($conll,5));
+
+	} // testNextTokensIntoAnnotationHasIAnntag()
+
+	public function testTokenInto2AnnotationsHasJoinedAnntags() {
+
+		$relations = array(); // no relations
+        // call args for two annotations on this same token
+        $annotations_by_id = $this->generateAnnotation_By_IdTestData();
+		// copy 1 anntotation to second one on this same <from,to>
+		$annotations_by_id[2] = $annotations_by_id[1];
+		$annotations_by_id[2]['id'] = 2; // correct annotation id 
+		$annotations_by_id[2]['name'] = '2nd'; // change ann type name 
+        $annotations = $this->generateAnnotationsFromAnnotations_By_Id($annotations_by_id);
+        // invoke tested method
+        list($conll,$json_builder) = $this->protectedMethod->invoke(new ConllAndJsonFactory(),$this->ccl,$this->tokens,$relations,$annotations,$this->tokens_ids,$annotations_by_id);
+
+        // token in line 3 should have two annTags
+		$expectedAnnTags = "B-nam_adj:B-2nd";
+        $this->assertEquals($expectedAnnTags,$this->extractAnntagsFromConllLines($conll,3));
+ 
+	} // testTokenInto2AnnotationsHasJoinedAnntags() 
 
 /* class ConllAndJsonFactory has only 1 function
     function exportToConllAndJson($file_path_without_ext, $ccl, $tokens, $relations, $annotations, $tokens_ids, $annotations_by_id)
