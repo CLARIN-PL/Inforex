f5a82d5e38456ad67d65070c4daacf408ca34629
Tomasz Naskręt <tomasz.naskret@pwr.edu.pl>
2024-10-08 13:25:03 +0200

    rebuild

--- local/cli-export-xml.php
+++ local/cli-export-xml.php
@@ -1,10 +1,16 @@
 <?php
+/**
+ * Part of the Inforex project
+ * Copyright (C) 2013 Michał Marcińczuk, Jan Kocoń, Marcin Ptak
+ * Wrocław University of Technology
+ * See LICENCE
+ */
 
 $enginePath = realpath(implode(DIRECTORY_SEPARATOR, array(dirname(__FILE__), "..", "engine")));
-require_once($enginePath. DIRECTORY_SEPARATOR . "settings.php");
-require_once($enginePath. DIRECTORY_SEPARATOR . 'include.php');
+require_once($enginePath . DIRECTORY_SEPARATOR . "settings.php");
+require_once($enginePath . DIRECTORY_SEPARATOR . 'include.php');
 Config::Config()->put_path_engine($enginePath);
-Config::Config()->put_localConfigFilename(realpath($enginePath . "/../config/").DIRECTORY_SEPARATOR."config.local.php");
+Config::Config()->put_localConfigFilename(realpath($enginePath . "/../config/") . DIRECTORY_SEPARATOR . "config.local.php");
 require_once($enginePath . "/cliopt.php");
 require_once($enginePath . "/clioptcommon.php");
 
@@ -12,68 +18,87 @@ mb_internal_encoding("utf-8");
 ob_end_clean();
 
 /******************** set configuration   *********************************************/
-
-define("PARAM_DB_URI", "db-uri");
 define("PARAM_DOCUMENT", "document");
 
 $opt = new Cliopt();
-$opt->addParameter(new ClioptParameter(PARAM_DB_URI, "U", "URI", "connection URI: user:pass@host:ip/name"));
-$opt->addParameter(new ClioptParameter(PARAM_DOCUMENT, "d", "id", "id of the document"));
+$opt->addParameter(new ClioptParameter("db-uri", "U", "URI", "connection URI: user:pass@host:ip/name"));
+$opt->addParameter(new ClioptParameter("verbose", "v", null, "verbose mode"));
+$opt->addParameter(new ClioptParameter(PARAM_DOCUMENT, "d", "id", "Document id"));
 
-/******************** parse cli *********************************************/
-try{
-    /** Parse cli parameters */
+try {
+    ini_set('memory_limit', '1024M');
     $opt->parseCli($argv);
-    Config::Config()->put_dsn(array(
-        'phptype' => 'mysql',
-        'username' => 'inforex',
-        'password' => 'password',
-        'hostspec' => 'db' . ":" . '3306',
-        'database' => 'inforex'
-    ));
+    $path = $opt->getRequired("input");
 
-    $dsn = CliOptCommon::parseDbParameters($opt, Config::Config()->get_dsn());
+    $dbHost = "db";
+    $dbUser = "inforex";
+    $dbPass = "password";
+    $dbName = "inforex";
+    $dbPort = "3306";
 
-    $documentIds = $opt->getParameters(PARAM_DOCUMENT);
-    $corpusId = null;
-
-    /** Setup database  */
-    $GLOBALS['db'] = new Database($dsn,false);
-    $logger = new GroupedLogger();
-    var_dump($GLOBALS['db']);
-    /** Validate parameters  */
-    CliOptCommon::validateDocumentId($documentIds, true);
-    echo "Parameters validation... OK\n";
-
-    $corpusId = 158;
-    $report = DbReport::getReportById($documentIds);
-    /** Process the request  */
-
-    //$htmlStr = ReportContent::getHtmlStr($report);
-    //$htmlStr = ReportContent::insertTokens($htmlStr, DbToken::getTokenByReportId($report['id']));
-
-    $content = $report['content'];
-    echo "Orginal xml\n";
-    var_dump($content);
-
-    $htmlStr = new HtmlStr2($content, true);
-    $sql = "SELECT * FROM reports_annotations WHERE report_id = ?";
-    $ans =  $GLOBALS['db']->fetch_rows($sql, array($report['id']));
-    foreach ($ans as $a){
-        try{
-            $htmlStr->insertTag(intval($a['from']), sprintf("<anb id=\"%d\" type=\"%s\"/>", $a['id'], $a['type']), $a['to']+1, sprintf("<ane id=\"%d\"/>", $a['id']), TRUE);
+    if ($opt->exists("db-uri")) {
+        $uri = $opt->getRequired("db-uri");
+        if (preg_match("/(.+):(.+)@(.*):(.*)\/(.*)/", $uri, $m)) {
+            $dbUser = $m[1];
+            $dbPass = $m[2];
+            $dbHost = $m[3];
+            $dbPort = $m[4];
+            $dbName = $m[5];
+            Config::Config()->put_dsn(array(
+                'phptype' => 'mysql',
+                'username' => $dbUser,
+                'password' => $dbPass,
+                'hostspec' => $dbHost . ":" . $dbPort,
+                'database' => $dbName
+            ));
+        } else {
+            throw new Exception("DB URI is incorrect. Given '$uri', but expected 'user:pass@host:port/name'");
         }
-        catch(Exception $ex){
-            $this->page->set("ex", $ex);
+    }
+    Config::Config()->put_verbose($opt->exists("verbose"));
+} catch (Exception $ex) {
+    print "!! " . $ex->getMessage() . " !!\n\n";
+    $opt->printHelp();
+    die("\n");
+}
+
+try {
+    $documentIds = $opt->getParameters(PARAM_DOCUMENT);
+    $loader = new CclLoader(Config::Config()->get_dsn(), Config::Config()->get_verbose());
+    $loader->load($documentIds);
+} catch (Exception $ex) {
+    print "Error: " . $ex->getMessage() . "\n";
+    print_r($ex);
+}
+sleep(1);
+
+/**
+ * Handle single request from tasks_documents.
+ */
+class CclLoader
+{
+
+    function __construct($dsn, $verbose)
+    {
+        $this->db = new Database($dsn, false);
+        $GLOBALS['db'] = $this->db;
+        $this->verbose = $verbose;
+    }
+
+    /**
+     * Print message if verbose mode is on.
+     */
+    function info($message)
+    {
+        if ($this->verbose) {
+            echo $message . "\n";
         }
     }
 
-    $content = $htmlStr->getContent();
-    var_dump($content);
-
-}catch(Exception $ex){
-    print "!! ". $ex->getMessage() . " !!\n\n";
-    $opt->printHelp();
-    print("\n");
-    return;
+    function load($report_id)
+    {
+        $doc = $this->db->fetch("SELECT * FROM reports WHERE id=?", array($report_id));
+        echo "Processing " . $report_id . "\n";
+        echo $doc["content"];
+    }
 }
