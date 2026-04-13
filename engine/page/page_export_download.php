ac4d605965b9fea8809b5c91c0d7a65aa8bbe84a
Tomasz Naskręt <tomasz.naskret@pwr.edu.pl>
2026-04-13 14:44:57 +0200

    download fix

--- engine/page/page_export_download.php
+++ engine/page/page_export_download.php
@@ -12,16 +12,27 @@ class Page_export_download extends CPage{
 	function execute(){
 		$export_id = intval($_GET['export_id']);
 		$file = Page_corpus_export::getExportFilePath($export_id);
-	    if (file_exists($file)) {
+	    if (is_file($file) && is_readable($file)) {
+			session_write_close();
+			set_time_limit(0);
+			ini_set('zlib.output_compression', 'Off');
 			while (ob_get_level() > 0) {
 				ob_end_clean();
 			}
-		    header('Content-Type: application/x-7z-compressed');
+		    header('Content-Type: application/octet-stream');
 		    header("Content-Disposition: attachment; filename=\"inforex_export_{$export_id}.7z\"");
 			header('Content-Transfer-Encoding: binary');
+			header('Content-Encoding: identity');
 			header('Content-Length: ' . filesize($file));
-			header('Cache-Control: private');
-		    readfile($file);
+			header('Cache-Control: no-store, no-cache, must-revalidate, no-transform');
+			header('Pragma: no-cache');
+			header('X-Accel-Buffering: no');
+			$handle = fopen($file, 'rb');
+			while (!feof($handle)) {
+				echo fread($handle, 1048576);
+				flush();
+			}
+			fclose($handle);
 	        exit();
 	    } else { // File not found
             // write to error log
